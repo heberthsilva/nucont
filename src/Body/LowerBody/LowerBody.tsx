8a0d967616c9b01c4b54234c2f051dc5c82ca453
Heberth Silva <heberth.silva@live.com>
2020-09-25 00:44:56 -0300

    Desemvolvimento LowerBody

--- src/Body/LowerBody/LowerBody.tsx
+++ src/Body/LowerBody/LowerBody.tsx
@@ -9,9 +9,26 @@ import './styles.css';
 function LowerBody() {
     return (
         <div>
-            <h1>teste</h1>
+            <h1 className="title_low_1 ">ESSA SÉRIE É PRA VOCÊ CONTADOR:</h1>
+            <ul className="ul_low">
+                <li className="li_low">Que quer ter processos contábeis ágeis </li>
+                <li className="li_low">Que quer aumentar o valor percebido pelo seu cliente e ajudá-lo na tomada de decisão</li>
+                <li className="li_low">Que deseja ver na prática como aplicar uma consultoria contábil eficaz e com método claro</li>
+            </ul>
+            <p className="p_low"></p>
+            <h1 className="title_low_2 ">O QUE SERÁ ABORDADO NA SÉRIE:</h1>
+            <ul className="ul_low2">
+                <li className="li_low">Análises de indicadores de empresas reais no contexto atual </li>
+                <li className="li_low">Doenças financeiras como insolvência, undertrading e excesso de endividamento</li>
+                <li className="li_low">Metodologia científica para aplicação de consultoria contábil </li>
+                <li className="li_low">Pesquisas e comparativos de mercado para entender o contexto das pequenas empresas na pandemia </li>
+            </ul>
+            <p className="p_low2"></p>
 
         </div>
+
+
+
 
     );
 }
